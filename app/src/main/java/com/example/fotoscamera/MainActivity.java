0c4668fb793d797e65f7c80f86082cd816452a5a
Jose <jrosillonieto@gmail.com>
2021-12-10 17:07:14 +0100

    App amb imatge full size i persistencia

--- app/src/main/java/com/example/fotoscamera/MainActivity.java
+++ app/src/main/java/com/example/fotoscamera/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.fotoscamera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,20 +19,24 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-
-
-
+    private ImageView imagev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn_foto = (Button) findViewById(R.id.btn_foto);
-        
+
+        imagev= findViewById(R.id.imageView);
+        Uri fileUri = Uri.fromFile(getFile());
+
+        imagev.setImageURI(fileUri);
+
         btn_foto.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -39,37 +47,68 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
-    public void dispatchTakePictureIntent() {
-
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
-
-        }
+    protected File getFile(){
+        // Guardar a un fitxer
+        File path = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File foto = new File(path, "image.jpg");
+        return foto;
     }
+
+
+    private void dispatchTakePictureIntent() {
+        Log.i("Funcio", "Entrant");
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+
+            Log.i("Funcio", "Entrant2");
+
+            // Create the File where the photo should go
+            File photoFile = getFile();
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.fotoscamera",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+
+            }
+        }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        final ImageView imageView = findViewById(R.id.imageView);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-            
-            File path = getApplicationContext().getFilesDir();
-            File foto = new File(path, "foto.jpg");
+            this.recreate();
 
-            try {
-                FileOutputStream os = new FileOutputStream(foto);
-                imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, os);
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
+
         }
+
+//    private void setPic() {
+//        // Get the dimensions of the View
+//        int targetW = imageView.getWidth();
+//        int targetH = imageView.getHeight();
+//
+//        // Get the dimensions of the bitmap
+//        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+//        bmOptions.inJustDecodeBounds = true;
+//
+//        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+//
+//        int photoW = bmOptions.outWidth;
+//        int photoH = bmOptions.outHeight;
+//
+//        // Determine how much to scale down the image
+//        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
+//
+//        // Decode the image file into a Bitmap sized to fill the View
+//        bmOptions.inJustDecodeBounds = false;
+//        bmOptions.inSampleSize = scaleFactor;
+//        bmOptions.inPurgeable = true;
+//
+//        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+//        imageView.setImageBitmap(bitmap);
+//    }
     }
-}
